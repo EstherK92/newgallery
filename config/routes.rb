5118bbed9014cf3053b4d50f3fa8daca2e52de2c
EstherK92 <essiek92@gmail.com>
2018-07-28 15:05:23 +0000

    artistscaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :artists
   devise_for :users
   resources :pictures
   root 'welcome#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727172151) do
+ActiveRecord::Schema.define(version: 20180728143826) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.string   "website"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "name"
