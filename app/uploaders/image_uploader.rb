d4d41b61c324f669c62226309b42da40d6bae9b6
Your Name <essiek92@.com>
2018-12-13 16:20:58 +0000

    images working when you click show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,10 @@
-#class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-# storage :file
+ storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -29,19 +29,22 @@
   # end
 
   # Create different versions of your uploaded files:
-   #version :thumb do
-    # process resize_to_fit: [50, 50]
-   #end
+   version :thumb do
+     process resize_to_fit: [500, 500]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_whitelist
     %w(jpg jpeg)
   end
+  
+  
+
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-#end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180809161720) do
+ActiveRecord::Schema.define(version: 20180808211304) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -47,11 +47,6 @@ ActiveRecord::Schema.define(version: 20180809161720) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "payments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "pictures", force: :cascade do |t|
     t.string   "name"
     t.integer  "width"
@@ -78,7 +73,6 @@ ActiveRecord::Schema.define(version: 20180809161720) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
